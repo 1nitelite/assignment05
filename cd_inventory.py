fccf96a722ea0cb1043a968f1af4cacceae224d4
nitelite <norlaila.toulas@gmail.com>
2022-05-18 01:09:58 -0700

    Created file, worked through each option to convert list to dictionary, used print debugging to work through errors

---
+++ cd_inventory.py
@@ -0,0 +1,63 @@
+"""
+Title: CD Inventory
+Desc: Displays menu to user to choose between adding CD info, display info, save to file, or exit
+Change Log: (Who, When, What)
+NToulas, 2022-May-17, Created File
+"""
+
+cd_data = {'ID': ['Artist Name', 'Album Name', 'Year Released']}
+# print(cd_data)
+
+cd_data['1234'] = ['Essam', '12.12', '2018']
+# print(cd_data)
+
+cd_file = 'cd_inventory.txt'
+
+ALBUM_ID_INDEX = 0
+ARTIST_NAME_INDEX = 1
+ALBUM_NAME_INDEX = 2
+ALBUM_YEAR_INDEX = 3
+
+name = input('Welcome! Please sign your name: ')
+print(f'Welcome to {name.title()}\'s Music Collection.')
+while True:
+    # displays menu
+    print('Please choose from the following options:')
+    print("1. View current inventory", "2. Add new album", "3. Delete album from inventory", "4. Save to file", "5. Exit", sep='\n')
+    user_input = input('What would you like to do? ')
+    import random
+    # option 1: display current inventory
+    if user_input == '1':
+        for key, value in cd_data.items():
+            print(f"{key} | {value}")
+    # option 2: add new album
+    elif user_input == '2':
+        album_id = random.randint(1000, 9999)
+        artist_name = input('Enter Artist Name: ')
+        album_name = input('Enter Album Name: ')
+        album_year = input('Enter Release Year (n/a if unknown): ')
+        cd_data[album_id] = [artist_name.title(), album_name.title(), album_year]
+    # option 3: delete album from inventory
+    elif user_input == '3':
+        del_album = input('Please enter album ID: ')
+        # try:
+        for key in cd_data.keys():
+            print(key)
+            if del_album == key:
+                del cd_data[key]
+                print(cd_data)
+            else:
+                print(cd_data)
+                break
+    # option 4: save data to file cd_inventory.txt
+    # elif user_input == '4':
+    #     del cd_data['ID']
+    #     with open('cd_inventory.txt', 'a') as file:
+    #         for row in data:
+    #             file.write(f"\n           ID: {row[ALBUM_ID_INDEX]}\n  Artist Name: {row[ARTIST_NAME_INDEX]}\n   Album Name: {row[ALBUM_NAME_INDEX]}\nYear Released: {row[YEAR_NUM_INDEX]}\n")
+    # option 5: exit program
+    elif user_input == '5':
+        print('Thank you for accessing your Music Collection. Goodbye!')
+        break
+    else:
+        print("That is not a valid input. Please try again.")
--- main.py
+++ main.py
@@ -1,65 +1,72 @@
-
-data = []
-with open('congress-terms.csv', 'r') as file:
-    # read this file
-    data = file.read()
+#
+# data = []
+# with open('congress-terms.csv', 'r') as file:
+#     # read this file
+#     data = file.read()
+# # print(data)
+#
+# data = data.split('\n')
+# del data[0]
+# # print(data)
+#
+# CONGRESS_INDEX = 0
+# FIRST_NAME_INDEX = 3
+# MIDDLE_NAME_INDEX = 4
+# LAST_NAME_INDEX = 5
+# AGE_INDEX = 12
+# counter = 0
+# while counter < len(data):
+#     # print("counter =", counter)
+#     # print(f"data[{counter}] =", data[counter])
+#     #calls data in row at index counter, split it at the comma, then reassign cell with new value
+#     data[counter] = data[counter].split(',')
+#     data[counter][AGE_INDEX] = float(data[counter][AGE_INDEX])
+#     # print(f"data[{counter}] =", data[counter])
+#     #move on to next row
+#     counter += 1
 # print(data)
-
-data = data.split('\n')
-del data[0]
-# print(data)
-
-CONGRESS_INDEX = 0
-FIRST_NAME_INDEX = 3
-MIDDLE_NAME_INDEX = 4
-LAST_NAME_INDEX = 5
-AGE_INDEX = 12
-counter = 0
-while counter < len(data):
-    # print("counter =", counter)
-    # print(f"data[{counter}] =", data[counter])
-    #calls data in row at index counter, split it at the comma, then reassign cell with new value
-    data[counter] = data[counter].split(',')
-    data[counter][AGE_INDEX] = float(data[counter][AGE_INDEX])
-    # print(f"data[{counter}] =", data[counter])
-    #move on to next row
-    counter += 1
-print(data)
-# grid = []
+# # grid = []
+# # for row in data:
+# #     grid.append(row.split(','))
+#
+#
+# min_age = 1000
+# min_congress_number = -1
+# min_name = ''
+# max_age = 1
+# max_congress_number = -1
+# max_name = ''
 # for row in data:
-#     grid.append(row.split(','))
+#     if row[AGE_INDEX] < min_age:
+#         min_age = row[AGE_INDEX]
+#         min_congress_number = row[CONGRESS_INDEX]
+#         min_name = f"{row[FIRST_NAME_INDEX]} {row[MIDDLE_NAME_INDEX]} {row[LAST_NAME_INDEX]}"
+#     if row[AGE_INDEX] > max_age:
+#         max_age = row[AGE_INDEX]
+#         max_congress_number = row[CONGRESS_INDEX]
+#         max_name = f"{row[FIRST_NAME_INDEX]} {row[MIDDLE_NAME_INDEX]} {row[LAST_NAME_INDEX]}"
+#
+# print(min_age)
+# print(min_congress_number)
+# print(min_name)
+# print(max_age)
+# print(max_congress_number)
+# print(max_name)
+#
+# answers = [
+#     [min_congress_number, str(min_age), min_name],
+#     [max_congress_number, str(max_age), max_name]
+# ]
+# with open('answers.csv', 'w+') as file:
+#     for answer in answers:
+#     # file.write(f'{min_congress_number},{min_age},{min_name}\n')
+#     # file.write(f'{max_congress_number},{max_age},{max_name}\n')
+#         file.write(','.join(answer))
+#         file.write('\n')
 
-
-min_age = 1000
-min_congress_number = -1
-min_name = ''
-max_age = 1
-max_congress_number = -1
-max_name = ''
-for row in data:
-    if row[AGE_INDEX] < min_age:
-        min_age = row[AGE_INDEX]
-        min_congress_number = row[CONGRESS_INDEX]
-        min_name = f"{row[FIRST_NAME_INDEX]} {row[MIDDLE_NAME_INDEX]} {row[LAST_NAME_INDEX]}"
-    if row[AGE_INDEX] > max_age:
-        max_age = row[AGE_INDEX]
-        max_congress_number = row[CONGRESS_INDEX]
-        max_name = f"{row[FIRST_NAME_INDEX]} {row[MIDDLE_NAME_INDEX]} {row[LAST_NAME_INDEX]}"
-
-print(min_age)
-print(min_congress_number)
-print(min_name)
-print(max_age)
-print(max_congress_number)
-print(max_name)
-
-answers = [
-    [min_congress_number, str(min_age), min_name],
-    [max_congress_number, str(max_age), max_name]
-]
-with open('answers.csv', 'w+') as file:
-    for answer in answers:
-    # file.write(f'{min_congress_number},{min_age},{min_name}\n')
-    # file.write(f'{max_congress_number},{max_age},{max_name}\n')
-        file.write(','.join(answer))
-        file.write('\n')
+sentence = 'The quick fox jumped over the lazy brown dog'
+word = 'jumped'
+# Let's try the IN operator
+print(word in sentence)
+assert word in sentence
+print('found it!')
