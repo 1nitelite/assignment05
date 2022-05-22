834e69f23d5c0c225ce56cd15e1e0d6263b0af3d
nitelite <norlaila.toulas@gmail.com>
2022-05-22 14:45:04 -0700

    Edited option 1 to load existing file as well as any added albums

--- cd_inventory.py
+++ cd_inventory.py
@@ -5,58 +5,62 @@ Change Log: (Who, When, What)
 NToulas, 2022-May-17, Created File
 """
 
-cd_data = {'ID': ['Artist Name', 'Album Name', 'Year Released']}
-# print(cd_data)
-
-cd_data['1234'] = ['Essam', '12.12', '2018']
-# print(cd_data)
-
+cd_data = {}
+cd_row = {}
+obj_file = None
 cd_file = 'cd_inventory.txt'
 
 ALBUM_ID_INDEX = 0
 ARTIST_NAME_INDEX = 1
 ALBUM_NAME_INDEX = 2
-ALBUM_YEAR_INDEX = 3
 
-name = input('Welcome! Please sign your name: ')
-print(f'Welcome to {name.title()}\'s Music Collection.')
+user_name = input('Welcome! Please login with your name: ')
+print(f'Hello {user_name.title()}! Welcome to your Music Collection.')
+
 while True:
     # displays menu
-    print('Please choose from the following options:')
-    print("1. View current inventory", "2. Add new album", "3. Delete album from inventory", "4. Save to file", "5. Exit", sep='\n')
+    print('\nPlease choose from the following options:')
+    print("1. View inventory", "2. Add new album", "3. Delete album", "4. Save to file", "5. Exit", sep='\n')
     user_input = input('What would you like to do? ')
-    import random
+
     # option 1: display current inventory
     if user_input == '1':
+        obj_file = open(cd_file, 'r')
+        for row in obj_file:
+            cd_row = row.strip().split(',')
+            cd_data[cd_row[ALBUM_ID_INDEX]] = [cd_row[ARTIST_NAME_INDEX], cd_row[ALBUM_NAME_INDEX]]
+        obj_file.close()
         for key, value in cd_data.items():
             print(f"{key} | {value}")
+
     # option 2: add new album
     elif user_input == '2':
+        import random
         album_id = str(random.randint(1000, 9999))
         artist_name = input('Enter Artist Name: ')
         album_name = input('Enter Album Name: ')
-        album_year = input('Enter Release Year (n/a if unknown): ')
-        cd_data[album_id] = [artist_name.title(), album_name.title(), album_year]
+        cd_data[album_id] = [artist_name.title(), album_name.title()]
+        print('Album has been successfully added!')
+
     # option 3: delete album from inventory
     elif user_input == '3':
         del_album = input('Please enter album ID: ')
         for key in cd_data.keys():
-            #if/el statement for checking to see if user inputs only numbers
             if key == del_album:
-                print("Album", cd_data[del_album], "will now be deleted")
+                print("Album", cd_data[del_album], "will now be deleted.")
                 cd_data.pop(del_album)
                 break
-            #
 
     # option 4: save data to file cd_inventory.txt
-    # elif user_input == '4':
-    #     del cd_data['ID']
-    #     with open('cd_inventory.txt', 'a') as file:
-    #         for row in data:
-    #             file.write(f"\n           ID: {row[ALBUM_ID_INDEX]}\n  Artist Name: {row[ARTIST_NAME_INDEX]}\n   Album Name: {row[ALBUM_NAME_INDEX]}\nYear Released: {row[YEAR_NUM_INDEX]}\n")
+    elif user_input == '4':
+         with open(cd_file, 'a') as file:
+            for key, values in cd_data.items():
+                file.write(f"\n{key},{values}")
+         print('Successfully saved!')
+
     # option 5: exit program
     elif user_input == '5':
-        print('Thank you for accessing your Music Collection. Goodbye!')
+        print(f'Thank you, {user_name.title()}, for accessing your Music Collection. Goodbye!')
         break
     else:
         print("That is not a valid input. Please try again.")
